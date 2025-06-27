fcd4ad7002dfe0811a7fee10af8c6f8c0329bac7
itsZeroFour <itsZeroFourX@gmail.com>
2025-06-27 23:22:17 +0300

    fix

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -5,7 +5,7 @@ import checkAuth from "../utils/checkAuth.js";
 const router = express.Router();
 
 router.post("/auth", UserControllers.userAutharization);
-router.get("/get", checkAuth, UserControllers.userGet);
+router.get("/get", UserControllers.userGet);
 router.patch("/update-attempts", UserControllers.addAttempts);
 
 export default router;
