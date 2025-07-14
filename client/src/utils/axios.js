8fcfd7e5b4295f2fd6b27d217c06c86e6c85f352
itsZeroFour <itsZeroFourX@gmail.com>
2025-07-14 13:21:49 +0300

    add token

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,20 +1,12 @@
 import axios from "axios";
 
-const getCookie = (name) => {
-  return (
-    document.cookie
-      .split("; ")
-      .find((row) => row.startsWith(`${name}=`))
-      ?.split("=")[1] ?? null
-  );
-};
-
 const instance = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = getCookie("token");
+  config.headers.Authorization =
+    "9trg3XDQtOeHbF3ZYwKPBqeGZ2fp6p0rkEkmHfedW8ftmCO2hoJ2IHsRjCZx3Fo";
 
   return config;
 });
