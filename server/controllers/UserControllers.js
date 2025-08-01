6d9ad59446edb8239d0f67dc3f02b989b5b23a7f
itsZeroFour <itsZeroFourX@gmail.com>
2025-08-01 11:41:17 +0300

    fix

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -223,18 +223,17 @@ export const activatePromocode = async (req, res) => {
       host: "mq-test.maxi-retail.ru",
       port: 61617,
       connectHeaders: {
-        host: "/",
+        host: "mq-test.maxi-retail.ru",
         login: process.env.LOGIN,
         passcode: process.env.PASSCODE,
-        "heart-beat": "5000,5000",
+        "accept-version": "1.2",
+        // "heart-beat": "5000,5000",
       },
-      // ssl: true,
-      // sslOptions: {
-      //   cert: fs.readFileSync("/etc/nginx/certs/fullchain.pem"),
-      //   key: fs.readFileSync("/etc/nginx/certs/privkey.pem"),
-      //   rejectUnauthorized: true,
-      //   secureProtocol: "TLSv1_2_method",
-      // },
+      ssl: true,
+      sslOptions: {
+        rejectUnauthorized: true,
+        secureProtocol: "TLSv1_2_method",
+      },
     };
 
     const promoData = {
