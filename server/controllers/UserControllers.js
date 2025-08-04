d7bb86efaf0c8c503c12cc42a4e5c3e8878a18ee
itsZeroFour <itsZeroFourX@gmail.com>
2025-08-04 14:29:26 +0300

    update

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -219,16 +219,19 @@ export const activatePromocode = async (req, res) => {
 
     const connectOptions = {
       host: "mq-test.maxi-retail.ru",
-      port: 61617,
+      port: 61613,
       connectHeaders: {
         host: "/",
         login: process.env.LOGIN,
         passcode: process.env.PASSCODE,
         "heart-beat": "5000,5000",
       },
-      ssl: true,
-      rejectUnauthorized: false,
-      checkServerIdentity: () => {},
+      ssl: false,
+      connectTimeout: 10000,
+      reconnectOptions: {
+        maxReconnects: 3,
+        retryDelay: 5000,
+      },
     };
 
     const promoData = {
