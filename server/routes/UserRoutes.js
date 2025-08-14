1c47c0eb9cd850876fd6af8ceaee7fd43c79f2bf
itsZeroFour <itsZeroFourX@gmail.com>
2025-08-14 09:10:51 +0300

    update routes

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,15 +1,26 @@
 import express from "express";
 import { UserControllers } from "../controllers/index.js";
-import checkAuth from "../utils/checkAuth.js";
 import checkToken from "../utils/checkToken.js";
 
 const router = express.Router();
 
-router.post("/auth", checkAuth, UserControllers.userAuthorization);
+router.post("/auth", checkToken, UserControllers.userAuthorization);
 router.get("/get", checkToken, UserControllers.userGet);
-router.post("/level-complete/:level", checkToken, UserControllers.levelComplete)
-router.post("/complete-onbording", checkToken, UserControllers.completeOnbording)  
-router.post("/activate-promocode", checkToken, UserControllers.activatePromocode)  
-router.post("/level-start", checkToken, UserControllers.levelStart)  
+router.post(
+  "/level-complete/:level",
+  checkToken,
+  UserControllers.levelComplete
+);
+router.post(
+  "/complete-onbording",
+  checkToken,
+  UserControllers.completeOnbording
+);
+router.post(
+  "/activate-promocode",
+  checkToken,
+  UserControllers.activatePromocode
+);
+router.post("/level-start", checkToken, UserControllers.levelStart);
 
 export default router;
